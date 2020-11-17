d78fff6ae5f5780a7a614a2385d25de512b39f8a
Victor <ParenVic@i.ua>
2020-11-17 11:00:18 +0200

    add font size to select text to Content.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,30 @@ import React, {FormEvent, useState} from 'react';
 import './App.css';
 import {Content} from './components/Content';
 
+type stateType = {
+    text: string
+    fontSize: string
+    color: string
+}
 
 function App() {
 
+    // const [value, setValue] = useState<stateType>({text: '',
+    // fontSize: '14px',
+    //     color: 'black'
+    // });
+
     const [text, setText] = useState('');
 
-    const [color, setColor] = useState('black');
-
-
+    const change = (st: string = '') => {
+        setText(st)
+        console.log(text)
+    }
 
   return (
       <div className="App">
         <h1>Text Editor with React Hooks</h1>
-          <Content text={text} setText={setText} />
-
-
+          <Content change={change} text={text} />
       </div>
   );
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,49 +1,108 @@
-import React, {useRef} from 'react';
+import React, {ChangeEvent,  useRef,} from 'react';
+import  './Content.css'
+
 type PropsType = {
     text: string
-    setText: (t: string) => void
+    change: (t: string) => void
 }
 
-export const Content = ({ text, setText}: PropsType) => {
+export const Content = ({change, text}: PropsType) => {
 
-    const changeText = (e: React.ChangeEvent<HTMLInputElement>):  void => {
-        setText(e.target.value);
-
-    }
-
-    const
+    const textRef = useRef(null)
 
 
 
-    const changeBackground = () => {
 
+    const changeText2 = (comand: string, e: React.ChangeEvent<HTMLInputElement>): void => {
+        debugger
+        change(e.target.value);
+        console.log(text)
     }
 
     const toJSON = () => {
-        text
+        const json = JSON.stringify(text)
+        debugger
+        console.log(json)
     }
+
+    const formatText = (sCnd: string, value:string): void => {
+         document.execCommand(sCnd, false, value)
+
+        document.execCommand('copy')
+        navigator.clipboard.readText().then(text=> {
+            const newObj = {
+                text: text,
+                fontSize: value,
+                color: 'red'
+            }
+
+            debugger
+        })
+
+    }
+
+    //execCommand(String aCommandName, Boolean aShowDefaultUI, String aValueArgument)
+
+    // const changeFontSize = (e: ChangeEvent<HTMLSelectElement>): void => {
+    //     console.log(e.currentTarget.value)
+    // }
+    //
+    // const changeColor = (e: ChangeEvent<HTMLSelectElement>): void => {
+    //     console.log(e.currentTarget.value)
+    // }
+    //
+    // const changeBackground = (e: ChangeEvent<HTMLSelectElement>): void => {
+    //     console.log(e.currentTarget.value)
+    // }
+
+    const fontSizeArray = [
+        "8px", "10px", "12px", "14px", "16px", "18px", "20px"
+    ]
 
     return (
 
         <>
-        <div contentEditable={'true'} style={{marginLeft: '100px',
-            minHeight: '500px', maxWidth: '1000px', outline: '0px solid transparent',
-            border: '0.5px solid #C6C6C6',
-            padding: '10px',
-            backgroundColor: 'white'}} > {text}
-        </div>
-            <div>
-                <button onChange={changeBackground}  >Change color</button>
-                <button>Change font-size</button>
-                <button>Change background</button>
-                <button onClick={toJSON}>To JSON-FORMAT</button>
+            <form onSubmit={toJSON} className={'form'}>
+                <select onChange={(e) =>formatText('fontSize', e.currentTarget.value)}>
+                    font size
+                    {fontSizeArray.map(f =>  <option key={f} value={f}>{f}</option>)}
+                </select >
+                {/*<select onChange={formatText}>*/}
+                {/*    <option value="red">red</option>*/}
+                {/*    <option value="green">green</option>*/}
+                {/*    <option value="yellow">yellow</option>*/}
+                {/*    <option value="blue">blue</option>*/}
+                {/*    <option value="black">black</option>*/}
+                {/*    <option value="greenyellow">greenyellow</option>*/}
+                {/*    <option value="pink">pink</option>*/}
+                {/*</select>*/}
+                {/*<select onChange={formatText}>*/}
+                {/*    <option value="red">red</option>*/}
+                {/*    <option value="green">green</option>*/}
+                {/*    <option value="yellow">yellow</option>*/}
+                {/*    <option value="blue">blue</option>*/}
+                {/*    <option value="black">black</option>*/}
+                {/*    <option value="greenyellow">greenyellow</option>*/}
+                {/*    <option value="pink">pink</option>*/}
+                {/*</select>*/}
+            </form>
+
+            <div contentEditable={'true'} style={{
+                marginLeft: '100px',
+                minHeight: '500px', maxWidth: '1000px', outline: '0px solid transparent',
+                border: '0.5px solid #C6C6C6',
+                padding: '10px',
+                backgroundColor: 'white'
+            }}
+                // onClick={changeText2}
+                //  onChange={changeText2}
+                ref={textRef}
+            > {text}
             </div>
-            </>
-
-
-
-
-
+            <div>
+                <button onClick={toJSON} /*onChange={changeText}*/>To JSON-FORMAT</button>
+            </div>
+        </>
 
     )
 }
