307ee67c850fd1b8499345833b555d39221c0b17
Victor <ParenVic@i.ua>
2020-11-18 18:43:57 +0200

    add gh-pages

--- src/components/Content/ChangeTextProperties/ChangeTextProterties.tsx
+++ src/components/Content/ChangeTextProperties/ChangeTextProterties.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import './ChangeTextProperties.css'
 
 type PropsType = {
-    /*formatFontSize(f: string, value: string ): void
-    formatColor(f: string, value: string ): void
-    formatBackground(f: string, value: string ): void*/
-    formatText(f: string, value: string ): void
-    toJSON: ()=> void
+    formatText(f: string, value: string): void
+    toJSON: () => void
 }
 const fontSizeArray = [
     {key: 0, value: 'font size'}, {key: 1, value: '10px'}, {key: 2, value: '12px'}, {
@@ -23,16 +20,17 @@ const bgColorArray = [
     'background color', 'red', 'green', 'yellow', 'blue', 'black', 'brown', 'greenyellow', 'pink'
 ]
 
-export const ChangeTextProperties = ({/*formatFontSize, formatColor, formatBackground,*/ formatText, toJSON}: PropsType) => {
+export const ChangeTextProperties = ({ formatText, toJSON}: PropsType) => {
     return (
         <>
-            <div className={'form'}>
-                <select className={'selectItems'} onChange={(e) => formatText('fontSize',  e.currentTarget[+e.currentTarget.value].innerHTML )}>
+            <div className={'selectFields'}>
+                <select className={'selectItems'}
+                        onChange={(e) => formatText('fontSize', e.currentTarget[+e.currentTarget.value].innerHTML)}>
                     font size
                     {fontSizeArray.map(f => <option key={f.key} value={f.key}>{f.value}</option>)}
                 </select>
                 <select className={'selectItems'}
-                        onChange={(e) => formatText('foreColor',  e.currentTarget.value)}> color
+                        onChange={(e) => formatText('foreColor', e.currentTarget.value)}> color
                     {colorArray.map(c => <option key={c} value={c}>{c}</option>)}
                 </select>
                 <select className={'selectItems'} onChange={(e) => formatText('backColor', e.currentTarget.value)}>
@@ -40,9 +38,9 @@ export const ChangeTextProperties = ({/*formatFontSize, formatColor, formatBackg
                     {bgColorArray.map(c => <option key={c} value={c}>{c}</option>)}
                 </select>
 
-                <button onClick={toJSON} className={'buttonToJSON'} >Convert to JSON</button>
+                <button onClick={toJSON} className={'buttonToJSON'}>Convert to JSON</button>
             </div>
         </>
-        )
-  };
+    )
+};
 
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,61 +6,55 @@ import {ChangeTextProperties} from './ChangeTextProperties/ChangeTextProterties'
 export const Content = () => {
 
 
-    const [onj, setOnj] = useState({
+    const [obj, setObj] = useState({
         text: '',
         fontSize: '10px',
-        color: '',
+        color: 'black',
         backgroundColor: 'black'
     })
 
 
     const formatText = (sCnd: string, value: string): void => {
+
+
+
         document.execCommand(sCnd, false, value)
-        document.execCommand('copy')
-        navigator.clipboard.readText().then(text => {
-
+        document.execCommand('copy');
+                navigator.clipboard.readText().then(text => {
             switch (sCnd) {
-                case 'fontSize' :  {
-                    let newObj = {...onj, fontSize: value, text}
-                    setOnj(newObj)
+                case 'fontSize': { 
+                    let newObj = {...obj, fontSize: value, text}
+                    setObj(newObj)
                     break
                 }
 
-                case 'foreColor' :  {
-                    let newObj = {...onj, color: value, text }
-                    setOnj(newObj)
+                case 'foreColor': {
+                    let newObj = {...obj, color: value, text}
+                    setObj(newObj)
                     break
                 }
-                case 'backColor' :  {
-                    let newObj = {...onj, backgroundColor: value, text }
-                    setOnj(newObj)
+                case 'backColor': {
+                    let newObj = {...obj, backgroundColor: value, text}
+                    setObj(newObj)
                     break
                 }
             }
-
-
-
-
         })
     }
 
     const toJSON = () => {
-        const json = JSON.stringify(onj)
+        const json = JSON.stringify(obj)
         console.log(json)
     }
-
-
 
     return (
 
         <>
-            <ChangeTextProperties  toJSON={toJSON}
-                formatText={formatText}/>
+            <ChangeTextProperties toJSON={toJSON} formatText={formatText}/>
             <div contentEditable={'true'}
                  className={'textarea'}>
             </div>
 
         </>
-
     )
 }
